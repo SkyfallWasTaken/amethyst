9b3b00121fefdf38e72791a0b86c07b1406ff712
SkyfallWasTaken <mahadkalam1234@gmail.com>
2022-10-28 12:52:20 +0100

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+  import "virtual:windi.css"
+  import "../app.css";
+</script>
+
+<main class="container">
+  <slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  //import Greet from "$lib/Greet.svelte";
+</script>
+
+<h1>Welcome to Tauri!</h1>
+
+<p>Click on the Tauri, Vite, and Svelte logos to learn more.</p>
